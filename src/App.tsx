c2853fa131902262bb4cb4df64ae7d45a0b02f09
Jesnie <jesniemagaling05@gmail.com>
2025-08-23 17:01:36 +0800

    initialize orderq

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Search from './pages/Search';
+import Menu from './pages/Menu';
+import FoodDetails from './pages/FoodDetails';
+import Cart from './pages/Cart';
+import Orders from './pages/Orders';
+import PaymentMethod from './pages/PaymentMethod';
+import OrderStatus from './pages/OrderStatus';
+import Receipt from './pages/Receipt';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/food/:id" element={<FoodDetails />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/payment-method" element={<PaymentMethod />} />
+        <Route path="/order-status" element={<OrderStatus />} />
+        <Route path="/receipt" element={<Receipt />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
