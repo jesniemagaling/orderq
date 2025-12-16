89e6eb6a86b9614bc226225638ad5297d02e0e96
Jesnie <jesniemagaling05@gmail.com>
2025-12-17 02:39:51 +0800

    fix: axios remove /api

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -3,7 +3,7 @@ import { toast } from 'react-toastify';
 
 // Create Axios instance
 const api = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
