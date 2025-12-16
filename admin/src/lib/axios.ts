3b0ea9b0604305f99a687301f8248a80beacd355
Jesnie <jesniemagaling05@gmail.com>
2025-12-17 03:13:34 +0800

    fix: axios at admin

--- admin/src/lib/axios.ts
+++ admin/src/lib/axios.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+const API_URL =
+  import.meta.env.VITE_API_URL || 'https://orderq-backend.onrender.com/api';
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
